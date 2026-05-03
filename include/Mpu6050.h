e0e6a603b9a29fdcbeec5977057a3f98c7f8828f
David Weinstein <davidwn49@gmail.com>
2026-05-03 12:40:36 +0300

    Make use MPU6050 arduino librarry

--- include/Mpu6050.h
+++ include/Mpu6050.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <Arduino.h>
-#include <Wire.h>
+#include <Adafruit_MPU6050.h>
 
 class Mpu6050 {
 public:
@@ -17,21 +17,7 @@ public:
   Rotation readRotation();
 
 private:
-  enum Register : uint8_t {
-    SampleRateDivider = 0x19,
-    Configuration = 0x1A,
-    GyroConfiguration = 0x1B,
-    GyroXOutHigh = 0x43,
-    PowerManagement1 = 0x6B,
-    WhoAmI = 0x75,
-  };
-
-  static constexpr float GyroScaleDegreesPerSecond = 131.0f;
-
-  void writeByte(uint8_t reg, uint8_t value);
-  uint8_t readByte(uint8_t reg);
-  void readBytes(uint8_t reg, uint8_t* buffer, size_t length);
-
-  TwoWire& wire;
+  Adafruit_MPU6050 mpu;
+  TwoWire* wire;
   uint8_t deviceAddress;
 };
--- src/Mpu6050.cpp
+++ src/Mpu6050.cpp
@@ -1,58 +1,28 @@
 #include "Mpu6050.h"
 
 Mpu6050::Mpu6050(TwoWire& wirePort, uint8_t address)
-    : wire(wirePort), deviceAddress(address) {}
+    : wire(&wirePort), deviceAddress(address) {}
 
 bool Mpu6050::begin() {
-  if (readByte(Register::WhoAmI) != 0x68) {
+  if (!mpu.begin(deviceAddress, wire)) {
     return false;
   }
 
-  writeByte(Register::PowerManagement1, 0x00);
-  delay(100);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
-  writeByte(Register::SampleRateDivider, 0x07);
-  writeByte(Register::Configuration, 0x00);
-  writeByte(Register::GyroConfiguration, 0x00);
   return true;
 }
 
 Mpu6050::Rotation Mpu6050::readRotation() {
-  uint8_t rawData[6] = {0};
-  readBytes(Register::GyroXOutHigh, rawData, sizeof(rawData));
+  mpu.getEvent(nullptr, nullptr, nullptr);
 
-  const int16_t rawX = static_cast<int16_t>((rawData[0] << 8) | rawData[1]);
-  const int16_t rawY = static_cast<int16_t>((rawData[2] << 8) | rawData[3]);
-  const int16_t rawZ = static_cast<int16_t>((rawData[4] << 8) | rawData[5]);
+  sensors_event_t gyro;
+  mpu.getEvent(nullptr, &gyro, nullptr);
 
   return Rotation{
-      rawX / GyroScaleDegreesPerSecond,
-      rawY / GyroScaleDegreesPerSecond,
-      rawZ / GyroScaleDegreesPerSecond,
+      gyro.gyro.x * 57.2958f,
+      gyro.gyro.y * 57.2958f,
+      gyro.gyro.z * 57.2958f,
   };
-}
-
-void Mpu6050::writeByte(uint8_t reg, uint8_t value) {
-  wire.beginTransmission(deviceAddress);
-  wire.write(reg);
-  wire.write(value);
-  wire.endTransmission();
-}
-
-uint8_t Mpu6050::readByte(uint8_t reg) {
-  uint8_t value = 0;
-  readBytes(reg, &value, 1);
-  return value;
-}
-
-void Mpu6050::readBytes(uint8_t reg, uint8_t* buffer, size_t length) {
-  wire.beginTransmission(deviceAddress);
-  wire.write(reg);
-  wire.endTransmission(false);
-  wire.requestFrom(static_cast<int>(deviceAddress), static_cast<int>(length));
-
-  size_t index = 0;
-  while (wire.available() && index < length) {
-    buffer[index++] = static_cast<uint8_t>(wire.read());
-  }
 }
